98f1a7dd93ede06d77741159e363d8216477ccaf
poseitutu10-main <phinehasoseitutu@gmil.com>
2026-01-03 02:45:53 -0800

    feat: added new arrivals to the product details

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/common/Header";
 import CategoryHeader from "@/features/homepage/components/CategoryHeader";
+import NewArrivals from "@/features/homepage/components/NewArrivals";
 import ProductDetail from "@/features/product-details/components/ProductDetail";
 
 const ProductPage = () => {
@@ -9,6 +10,7 @@ const ProductPage = () => {
       <div className=" mx-auto px-4 sm:px-6 lg:px-20">
         <CategoryHeader />
         <ProductDetail />
+        <NewArrivals />
       </div>
     </>
   );
